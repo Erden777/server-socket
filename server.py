5b4f0e0b3853d75e94628db179682fd66cea1842
madiyar.omirzakov <huistowns@gmail.com>
2021-04-09 18:16:56 +0600

    fix

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import main
 import client
 
 def start(selector):
-    print('this')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('localhost', 5001))
